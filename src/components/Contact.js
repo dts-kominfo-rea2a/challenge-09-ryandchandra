83f4c78f5eea4374b717ae3d4fe5d96518f47a79
ryandchandra <ryandchandra1@gmail.com>
2022-06-26 19:01:46 +0800

    feat: finish Contact and Header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,20 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+import "./Contact.css";
+
+const Contact = ({ contacts }) => {
     return (
-        <></>
+        <div className="contacts-container">
+            {contacts.map((contact) => <div className="contact-card" key={contact.name}>
+                <img src={contact.photo} alt="" />
+                <div className="contact-card-content">
+                    <h3>{contact.name}</h3>
+                    <p>{contact.phone}</p>
+                    <p>{contact.email}</p>
+                </div>
+            </div>)}
+        </div>
     )
 }
 
