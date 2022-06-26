077779431fa996ce42eb301d02a8576b6f89633b
ryandchandra <ryandchandra1@gmail.com>
2022-06-26 20:07:46 +0800

    fix: add key to Contact map at App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="App">
       <Header />
-      {contacts.map((contact) => <Contact data={contact} />)}
+      {contacts.map((contact) => <Contact data={contact} key={contact.name} />)}
     </div>
   );
 }
