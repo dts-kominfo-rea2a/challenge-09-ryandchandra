ef2a47ff6b72e7596f08af2c26501f59a378fa0c
ryandchandra <ryandchandra1@gmail.com>
2022-06-26 19:05:20 +0800

    fix: change Contact props name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,10 @@ import React from 'react';
 
 import "./Contact.css";
 
-const Contact = ({ contacts }) => {
+const Contact = ({ data }) => {
     return (
         <div className="contacts-container">
-            {contacts.map((contact) => <div className="contact-card" key={contact.name}>
+            {data.map((contact) => <div className="contact-card" key={contact.name}>
                 <img src={contact.photo} alt="" />
                 <div className="contact-card-content">
                     <h3>{contact.name}</h3>
